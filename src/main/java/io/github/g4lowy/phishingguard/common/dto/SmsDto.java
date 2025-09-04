5b788ff2c569f2bd476f57f82e8772197abc6836
Galowy <matgalv2@gmail.com>
2025-09-04 22:26:33 +0200

    Updated validation

--- src/main/java/io/github/g4lowy/phishingguard/common/dto/SmsDto.java
+++ src/main/java/io/github/g4lowy/phishingguard/common/dto/SmsDto.java
@@ -3,10 +3,11 @@ package io.github.g4lowy.phishingguard.common.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-public record SmsDto(@Pattern(regexp = "\\d{9}", message = PHONE_NUMBER_VALIDATION_MESSAGE) String sender,
-                     @Pattern(regexp = "\\d{9}", message = PHONE_NUMBER_VALIDATION_MESSAGE) String recipient,
+public record SmsDto(@Pattern(regexp = PHONE_NUMBER_PATTERN, message = PHONE_NUMBER_VALIDATION_MESSAGE) String sender,
+                     @Pattern(regexp = PHONE_NUMBER_PATTERN, message = PHONE_NUMBER_VALIDATION_MESSAGE) String recipient,
                      @NotBlank(message = MESSAGE_VALIDATION_MESSAGE) String message) {
 
+    private static final String PHONE_NUMBER_PATTERN = "\\d{9}";
     private static final String PHONE_NUMBER_VALIDATION_MESSAGE = "Phone number should consists of 9 numbers";
     private static final String MESSAGE_VALIDATION_MESSAGE = "Message should not be blank";
 }
