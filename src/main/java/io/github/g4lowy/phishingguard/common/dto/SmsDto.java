69ea197bdc7bb1f30148e12fd7cf12cd5fe46c13
g4lowy <matgalv2@gmail.com>
2025-09-07 22:02:10 +0200

    Deleted unnecessary variable

--- src/main/java/io/github/g4lowy/phishingguard/common/dto/SmsDto.java
+++ src/main/java/io/github/g4lowy/phishingguard/common/dto/SmsDto.java
@@ -5,9 +5,8 @@ import jakarta.validation.constraints.Pattern;
 
 public record SmsDto(@Pattern(regexp = PHONE_NUMBER_PATTERN, message = PHONE_NUMBER_VALIDATION_MESSAGE) String sender,
                      @Pattern(regexp = PHONE_NUMBER_PATTERN, message = PHONE_NUMBER_VALIDATION_MESSAGE) String recipient,
-                     @NotBlank(message = MESSAGE_VALIDATION_MESSAGE) String message) {
+                     @NotBlank(message = "Message should not be blank") String message) {
 
     private static final String PHONE_NUMBER_PATTERN = "\\d{9}";
     private static final String PHONE_NUMBER_VALIDATION_MESSAGE = "Phone number should consists of 9 numbers";
-    private static final String MESSAGE_VALIDATION_MESSAGE = "Message should not be blank";
 }
