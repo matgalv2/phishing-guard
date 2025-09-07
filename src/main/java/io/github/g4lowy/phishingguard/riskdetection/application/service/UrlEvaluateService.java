bc7f534af8963a3843cb731b8275e6608cefaf88
g4lowy <matgalv2@gmail.com>
2025-09-07 22:13:49 +0200

    Fixed style

--- src/main/java/io/github/g4lowy/phishingguard/riskdetection/application/service/UrlEvaluateService.java
+++ src/main/java/io/github/g4lowy/phishingguard/riskdetection/application/service/UrlEvaluateService.java
@@ -1,21 +1,20 @@
 package io.github.g4lowy.phishingguard.riskdetection.application.service;
 
 
-import io.github.g4lowy.phishingguard.riskdetection.domain.model.Risk;
 import io.github.g4lowy.phishingguard.riskdetection.application.port.out.CachePort;
 import io.github.g4lowy.phishingguard.riskdetection.application.port.out.WebRiskPort;
+import io.github.g4lowy.phishingguard.riskdetection.domain.model.Risk;
 import io.github.g4lowy.phishingguard.riskdetection.domain.riskchecker.UrlRiskCheckerStrategy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class UrlEvaluateService {
+class UrlEvaluateService {
 
     private final WebRiskPort webRisk;
     private final CachePort cache;
@@ -25,7 +24,7 @@ public class UrlEvaluateService {
 
         return Flux.fromIterable(urls)
                 .flatMap(this::evaluateUrl)
-                .filter(risk -> risk == Risk.MALICIOUS)
+                .filter(risk -> risk != Risk.SAFE)
                 .next()
                 .defaultIfEmpty(Risk.SAFE);
     }
@@ -40,30 +39,24 @@ public class UrlEvaluateService {
         return cache.getUrl(urlKey)
                 .switchIfEmpty(cache.getETldPlusOne(e2ld))
                 .switchIfEmpty(heuristicOrApi(normalized)
-                    .flatMap(risk -> cache.putUrl(urlKey, risk, cacheTimeToLive(risk))
-                            .then(cache.putETldPlusOne(e2ld, risk, cacheTimeToLive(risk)))
-                            .thenReturn(risk)
-                    )
+                        .flatMap(risk -> cache.putUrl(urlKey, risk)
+                                .then(cache.putETldPlusOne(e2ld, risk))
+                                .thenReturn(risk)
+                        )
                 );
     }
 
-    private Mono<Risk> heuristicOrApi(String url){
+    private Mono<Risk> heuristicOrApi(String url) {
 
-        boolean suspiciousUrl = urlRiskCheckerStrategies.stream().anyMatch(strategy -> strategy.breaksRule(url));
+        boolean suspiciousUrl = urlRiskCheckerStrategies
+                .stream()
+                .anyMatch(strategy -> strategy.breaksRule(url));
 
-        if (!suspiciousUrl){
+        if (!suspiciousUrl) {
 
             return Mono.just(Risk.SAFE);
         }
 
-        return webRisk.evaluate(url).onErrorResume(error -> Mono.just(Risk.SAFE));
-    }
-
-    private long cacheTimeToLive(Risk risk){
-
-        return switch (risk) {
-            case SAFE -> Duration.ofHours(48).toSeconds();
-            case MALICIOUS -> Duration.ofDays(31).toSeconds();
-        };
+        return webRisk.evaluate(url);
     }
 }
