258f0c973e8b1fc76508efcaa54a5f964d9e4695
g4lowy <matgalv2@gmail.com>
2025-09-07 21:59:56 +0200

    Added helping method

--- src/main/java/io/github/g4lowy/phishingguard/riskdetection/adapter/out/web/dto/ThreatType.java
+++ src/main/java/io/github/g4lowy/phishingguard/riskdetection/adapter/out/web/dto/ThreatType.java
@@ -1,9 +1,17 @@
 package io.github.g4lowy.phishingguard.riskdetection.adapter.out.web.dto;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum ThreatType {
 
     THREAT_TYPE_UNSPECIFIED,
     SOCIAL_ENGINEERING,
     MALWARE,
-    UNWANTED_SOFTWARE
+    UNWANTED_SOFTWARE;
+
+    public static List<ThreatType> valuesAsList() {
+
+        return Arrays.stream(values()).toList();
+    }
 }
--- src/main/java/io/github/g4lowy/phishingguard/riskdetection/domain/model/Decision.java
+++ src/main/java/io/github/g4lowy/phishingguard/riskdetection/domain/model/Decision.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 public record Decision(Disposition disposition, Reason reason, List<String> checkedUrls) {
 
-    public static Decision allow(Reason reason, List<String> urls){
+    public static Decision allow(Reason reason, List<String> urls) {
 
-        return new Decision(Disposition.ALLOWED,  reason, urls);
+        return new Decision(Disposition.ALLOWED, reason, urls);
     }
 
-    public static Decision deny(Reason reason, List<String> urls){
+    public static Decision block(Reason reason, List<String> urls) {
 
-        return new Decision(Disposition.DENIED,  reason, urls);
+        return new Decision(Disposition.BLOCKED, reason, urls);
+    }
+
+    public String getDispositionName() {
+
+        return disposition.name();
+    }
+
+    public String getReasonMessage() {
+        return reason.getMessage();
     }
 }
