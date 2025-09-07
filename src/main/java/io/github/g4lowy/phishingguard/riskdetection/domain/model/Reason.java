4ec23af4bb858a16ed73651fd177ca5150ec0cf0
g4lowy <matgalv2@gmail.com>
2025-09-07 22:03:54 +0200

    Added new Reasons

--- src/main/java/io/github/g4lowy/phishingguard/riskdetection/domain/model/Reason.java
+++ src/main/java/io/github/g4lowy/phishingguard/riskdetection/domain/model/Reason.java
@@ -1,6 +1,19 @@
 package io.github.g4lowy.phishingguard.riskdetection.domain.model;
 
+import lombok.Getter;
+
+@Getter
 public enum Reason {
 
-    NO_URLS, MALICIOUS_URL, NO_MALICIOUS_URL_FOUND
+    NO_URLS("No urls found in message."),
+    SUSPICIOUS_URL("Url(s) look(s) suspicious, but could not be checked via webRisk service."),
+    MALICIOUS_URL("Malicious url(s) was/were found in message."),
+    NO_MALICIOUS_URL_FOUND("No malicious url(s) was/were found in message."),
+    INACTIVE_SUBSCRIPTION("All the traffic is allowed when subscription is inactive");
+
+    private final String message;
+
+    Reason(String message) {
+        this.message = message;
+    }
 }
